b7d53334b29d936436db1b5bebad96618bce3967
RuslanaLos <ruslanalos74@gmail.com>
2023-06-13 23:58:51 +0300

    section photo skill

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 $(function () {
     $("#tabs").tabs();
 });
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        animateOut: 'slideOutUp',
+        animateIn: 'slideInUp',
+        navText: [ '', ' ' ],
+        nav: false,
+        dots: true,
+
+
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1100:{
+                items:1
+            }
+        }
+    });
+});
