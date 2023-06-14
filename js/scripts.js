dd8860965508e874969ac2b17cec374603a46645
Anton Anikonov <antonanikonov1@gmail.com>
2023-06-14 17:39:08 +0300

    Js js js js

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,36 @@
-$(function () {
-    $("#tabs").tabs();
+ $(document).ready(function () {
+                $('.owl-carousel').owlCarousel({
+                        loop: true,
+                        autoplay: false,
+                        autoplayTimeout: 2000,
+                        animateOut: 'slideOutUp',
+                        animateIn: 'slideInUp',
+                        navText: ['', ' '],
+                        nav: false,
+                        dots: true,
+
+                responsive:{
+                    0
+                :
+                    {
+                        items:1
+                    }
+                ,
+                    768
+                :
+                    {
+                            items:2
+                            },
+            1100
+                    :
+                        {
+                            items:2
+                        }
+                    }
+                }
+            )
+     $(function () {
+         $("#tabs").tabs();
+     });
 });
-<<<<<<< HEAD
 
-
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        autoplay:true,
-        autoplayTimeout:3000,
-        navText: [ '', ' ' ],
-=======
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        autoplay:false,
-        autoplayTimeout:2000,
-        animateOut: 'slideOutUp',
-        animateIn: 'slideInUp',
-        navText: [ '', ' ' ],
-        nav: false,
-        dots: true,
-
->>>>>>> origin/master
-
-        responsive:{
-            0:{
-                items:1
-            },
-            768:{
-<<<<<<< HEAD
-                items:2
-            },
-            1100:{
-                items:2
-            }
-        }
-    });
-});
-=======
-                items:1
-            },
-            1100:{
-                items:1
-            }
-        }
-    });
-});
->>>>>>> origin/master
