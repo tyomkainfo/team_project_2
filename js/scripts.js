8f654306c4e17234f30c6286b79b33233123851b
StasKashperov <staskashperov96@gmail.com>
2023-06-14 16:31:17 +0300

    half

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(function () {
     $("#tabs").tabs();
 });
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1100:{
+                items:2
+            }
+        }
+    });
+});
