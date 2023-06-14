e03939e61c4cf0956ab250ddba14367fe6ba121a
Anton Anikonov <antonanikonov1@gmail.com>
2023-06-14 19:53:53 +0300

    add featured topics html and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,29 @@
- $(document).ready(function () {
-                $('.owl-carousel').owlCarousel({
-                        loop: true,
-                        autoplay: false,
-                        autoplayTimeout: 2000,
-                        animateOut: 'slideOutUp',
-                        animateIn: 'slideInUp',
-                        navText: ['', ' '],
-                        nav: false,
-                        dots: true,
 
-                responsive:{
-                    0
-                :
-                    {
-                        items:1
-                    }
-                ,
-                    768
-                :
-                    {
-                            items:2
-                            },
-            1100
-                    :
-                        {
-                            items:2
-                        }
-                    }
-                }
-            )
-     $(function () {
-         $("#tabs").tabs();
-     });
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:false,
+        autoplayTimeout:2000,
+        animateOut: 'slideOutUp',
+        animateIn: 'slideInUp',
+        navText: [ '', ' ' ],
+        nav: false,
+        dots: true,
+
+
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            1100:{
+                items:1
+            }
+        }
+    });
+    $(function () {
+        $("#tabs").tabs();
+    });
 });
-
